86eefd466f8316032a98e65479181ab96a28bd7e
Joshy20XX <vze3hw7c@gmail.com>
2023-07-24 12:28:11 -0400

    Added the cheese block recipe 🧀
    yum yum

---
+++ src/main/java/net/mcreator/newendupdate/block/CheeseBlockBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.newendupdate.block;
+
+import net.minecraftforge.common.IPlantable;
+
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.newendupdate.init.NewEndUpdateModItems;
+
+import java.util.List;
+import java.util.Collections;
+
+public class CheeseBlockBlock extends Block {
+	public CheeseBlockBlock() {
+		super(BlockBehaviour.Properties.of(Material.SPONGE).sound(SoundType.SAND).strength(1f, 10f));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 15;
+	}
+
+	@Override
+	public boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction direction, IPlantable plantable) {
+		return true;
+	}
+
+	@Override
+	public boolean isLadder(BlockState state, LevelReader world, BlockPos pos, LivingEntity entity) {
+		return true;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(NewEndUpdateModItems.CHEESE.get()));
+	}
+}
