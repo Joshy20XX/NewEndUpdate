90323399a44f10e72ca16cf1ff9c2507396cf544
Joshy20XX <vze3hw7c@gmail.com>
2023-07-24 14:10:16 -0400

    Change Ender Slasher Attack speed

--- src/main/java/net/mcreator/newendupdate/item/EnderSlasherItem.java
+++ src/main/java/net/mcreator/newendupdate/item/EnderSlasherItem.java
@@ -41,7 +41,7 @@ public class EnderSlasherItem extends SwordItem {
 			public Ingredient getRepairIngredient() {
 				return Ingredient.of();
 			}
-		}, 3, -3f, new Item.Properties());
+		}, 3, 76f, new Item.Properties());
 	}
 
 	@Override
