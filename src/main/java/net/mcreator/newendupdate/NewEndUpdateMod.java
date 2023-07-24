7be90e89ccf9b6762abf0de673ca2219c9a9d039
GibusRat <Kalc36092@gmail.com>
2023-07-24 09:32:36 -0700

    end portal

--- src/main/java/net/mcreator/newendupdate/NewEndUpdateMod.java
+++ src/main/java/net/mcreator/newendupdate/NewEndUpdateMod.java
@@ -30,6 +30,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
 import net.mcreator.newendupdate.init.NewEndUpdateModItems;
+import net.mcreator.newendupdate.init.NewEndUpdateModBlocks;
 
 import java.util.function.Supplier;
 import java.util.function.Function;
@@ -49,6 +50,7 @@ public class NewEndUpdateMod {
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+		NewEndUpdateModBlocks.REGISTRY.register(bus);
 		NewEndUpdateModItems.REGISTRY.register(bus);
 
 	}
---
+++ src/main/java/net/mcreator/newendupdate/init/NewEndUpdateModBlocks.java
@@ -0,0 +1,19 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.newendupdate.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.level.block.Block;
+
+import net.mcreator.newendupdate.block.EndideaPortalBlock;
+import net.mcreator.newendupdate.NewEndUpdateMod;
+
+public class NewEndUpdateModBlocks {
+	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, NewEndUpdateMod.MODID);
+	public static final RegistryObject<Block> ENDIDEA_PORTAL = REGISTRY.register("endidea_portal", () -> new EndideaPortalBlock());
+}
